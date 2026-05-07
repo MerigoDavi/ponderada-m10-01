172e7347f39c34b57dec33cfa6870b40775afff5
Murilo Carvalho <carvalho.zanini@gmail.com>
2026-05-07 17:52:29 -0300

    adicionado base da atividade

---
+++ app/src/main/java/carvalho/zanini/ponderada1/MainActivity.kt
@@ -0,0 +1,85 @@
+package carvalho.zanini.ponderada1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.random.Random
+
+import androidx.compose.ui.tooling.preview.Preview
+import carvalho.zanini.ponderada1.ui.theme.Ponderada1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LancadorDeDadosApp()
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun LancadorDeDadosApp() {
+    var dadoSelecionado by remember { mutableStateOf("D6") }
+    var resultado by remember { mutableStateOf("Clique no botão para lançar o dado") }
+
+    val dados = listOf("D6",)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Lançador de Dados",
+            fontSize = 28.sp
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(text = "Escolha o tipo de dado:")
+
+        dados.forEach { dado ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = dadoSelecionado == dado,
+                    onClick = { dadoSelecionado = dado }
+                )
+                Text(text = dado)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                val valorSorteado = when (dadoSelecionado) {
+                    "D6" -> Random.nextInt(6)
+                    else -> 0
+                }
+
+                resultado = "Resultado do $dadoSelecionado: $valorSorteado"
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Lançar dado")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = resultado,
+            fontSize = 20.sp
+        )
+    }
+}
